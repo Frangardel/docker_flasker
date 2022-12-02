38056dae9cd494ea8a38b027943ba0a98d98ecd6
Francisco García Delgado <frangardel@gmail.com>
2022-12-02 20:14:32 +0100

    update

--- app/app.py
+++ app/app.py
@@ -1,7 +1,5 @@
 from flask import Flask, jsonify
 from sqlalchemy import create_engine
-from datetime import datetime
-from flask_restx import Api, Namespace, Resource, reqparse, inputs, fields
 
 user = "schedulin"
 passw = "MySQLIsFun"
